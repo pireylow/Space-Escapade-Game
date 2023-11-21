9a5df45ee68da9d986237d2737ce911cc0f29270
pireylow <pirey.low@gmail.com>
2023-11-21 01:41:41 -0500

    initial

--- space-escapade/game.py
+++ space-escapade/game.py
@@ -16,3 +16,40 @@
 # 1. update -- move enemies and user, check for collisions
 # 2. power-up collision
 # 3. enemy collision
+
+import random
+
+# Global Variables
+startX = 200
+startY = 200
+
+class User:
+    def __init__(self):
+        self.position = (startX, startY)
+        
+    def move(self):
+        pass
+    
+class Enemies:
+    def __init__(self):
+        self.positions = []
+        
+    def move(self):
+        # pathfinding algorithm
+        pass
+    
+class GameMap:
+    def __init__(self):
+        self.maps = []
+        self.map = self.maps[random.randrange(0,5)]
+        
+    def changeMap(self):
+        self.map = self.maps[random.randrange(0,5)]
+    
+class Game:
+    def __init__(self):
+        self.user = User().position
+        self.enemies = Enemies().positions
+        self.map = GameMap().map
+
+    
--- space-escapade/main.py
+++ space-escapade/main.py
@@ -10,3 +10,103 @@ import game
 import graphics
 import pathfinding
 
+def onAppStart(app):
+    reset(app)
+    app.highscore = 0
+    
+def reset(app):
+    app.height = 1000
+    app.width = 1500
+    app.startScreen = True
+    app.paused = False
+    app.gameOver = False
+    app.game = False
+    app.score = 0
+
+    
+def redrawAll(app):
+    if app.startScreen:
+        drawStartScreen(app)
+    elif app.paused:
+        drawPauseScreen(app)
+    elif app.gameOver:
+        drawGameOverScreen(app)
+    else:
+        drawGame(app)
+    
+    
+def drawStartScreen(app):
+    pass       
+
+def drawPauseScreen(app):
+    pass
+
+def drawGameOverScreen(app):
+    pass
+
+def drawGame(app):
+    pass
+        
+        
+def onStep(app):
+    if app.game:
+        game.Enemies.move()
+    
+def onMouseDrag(app,mouseX,mouseY):
+    if app.game:
+        game.User.position = (mouseX,mouseY)
+    
+def onMousePress(app,mouseX,mouseY):
+    if app.startScreen:
+        if insideStartGameButton:
+            app.startScreen = not app.startScreen
+            app.game = not app.game
+            startGame()
+    elif app.paused:
+        if insideResumeButton or insideTopPlayButton:
+            app.paused = not app.paused
+            app.game = not app.game
+        elif insideQuitButton:
+            app.startScreen = not app.startScreen
+            app.paused = not app.paused
+    elif app.gameOver:
+        if insideNewGameButton:
+            reset(app)
+    else:
+        if insidePauseButton:
+            app.paused = not app.paused
+            app.game = not app.game
+    
+def onKeyPress(app,key):
+    if app.startScreen:
+        if key in ['space','enter','p']:
+            app.startScreen = not app.startScreen
+            app.game = not app.game
+            startGame()
+    elif app.paused:
+        if key in ['space','p']:
+            app.paused = not app.paused
+            app.game = not app.game
+    elif app.gameOver:
+        if key in ['space','n','r']:
+            reset(app)
+    else:
+        if key in ['space','p']:
+            app.paused = not app.paused
+            app.game = not app.game
+    
+def onKeyHold(app,keys):
+    if app.game:
+        if 'w' in keys or 'up' in keys:
+            game.User.position[2] -= 2
+        if 'a' in keys or 'left' in keys:
+            game.User.position[1] -= 2
+        if 's' in keys or 'down' in keys:
+            game.User.position[2] += 2
+        if 'd' in keys or 'right' in keys:
+            game.User.position[1] += 2
+
+def main():
+    runApp()
+
+main()
